47d2f70029a04c26edc9f6d7defcdf138e5aa491
Gorchon <a01254831@tec.mx>
2025-03-13 14:14:30 -0600

    Dev: Docs

--- MtdrSpring/backend/src/main/frontend/src/App.js
+++ MtdrSpring/backend/src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-          /*
+/*
 ## MyToDoReact version 1.0.
 ##
 ## Copyright (c) 2022 Oracle, Inc.
@@ -37,39 +37,32 @@ function App() {
     const [error, setError] = useState();
 
     function deleteItem(deleteId) {
-      // console.log("deleteItem("+deleteId+")")
-      fetch(API_LIST+"/"+deleteId, {
+      fetch(API_LIST + '/' + deleteId, {
         method: 'DELETE',
       })
       .then(response => {
-        // console.log("response=");
-        // console.log(response);
         if (response.ok) {
-          // console.log("deleteItem FETCH call is ok");
-          return response;
+          const remainingItems = items.filter(item => item.id !== deleteId);
+          setItems(remainingItems);
         } else {
           throw new Error('Something went wrong ...');
         }
       })
-      .then(
-        (result) => {
-          const remainingItems = items.filter(item => item.id !== deleteId);
-          setItems(remainingItems);
-        },
-        (error) => {
-          setError(error);
-        }
-      );
+      .catch(err => {
+        setError(err);
+      });
     }
+
     function toggleDone(event, id, description, done) {
       event.preventDefault();
       modifyItem(id, description, done).then(
-        (result) => { reloadOneIteam(id); },
+        () => { reloadOneItem(id); },
         (error) => { setError(error); }
       );
     }
-    function reloadOneIteam(id){
-      fetch(API_LIST+"/"+id)
+
+    function reloadOneItem(id){
+      fetch(API_LIST + '/' + id)
         .then(response => {
           if (response.ok) {
             return response.json();
@@ -79,22 +72,20 @@ function App() {
         })
         .then(
           (result) => {
-            const items2 = items.map(
-              x => (x.id === id ? {
-                 ...x,
-                 'description':result.description,
-                 'done': result.done
-                } : x));
-            setItems(items2);
+            const updatedItems = items.map(x =>
+              x.id === id
+                ? { ...x, description: result.description, done: result.done }
+                : x
+            );
+            setItems(updatedItems);
           },
-          (error) => {
-            setError(error);
-          });
+          (error) => { setError(error); }
+        );
     }
+
     function modifyItem(id, description, done) {
-      // console.log("deleteItem("+deleteId+")")
-      var data = {"description": description, "done": done};
-      return fetch(API_LIST+"/"+id, {
+      const data = { description, done };
+      return fetch(API_LIST + '/' + id, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json'
@@ -102,25 +93,16 @@ function App() {
         body: JSON.stringify(data)
       })
       .then(response => {
-        // console.log("response=");
-        // console.log(response);
         if (response.ok) {
-          // console.log("deleteItem FETCH call is ok");
           return response;
         } else {
           throw new Error('Something went wrong ...');
         }
       });
     }
-    /*
-    To simulate slow network, call sleep before making API calls.
-    const sleep = (milliseconds) => {
-      return new Promise(resolve => setTimeout(resolve, milliseconds))
-    }
-    */
+
     useEffect(() => {
       setLoading(true);
-      // sleep(5000).then(() => {
       fetch(API_LIST)
         .then(response => {
           if (response.ok) {
@@ -137,43 +119,31 @@ function App() {
           (error) => {
             setLoading(false);
             setError(error);
-          });
+          }
+        );
+    }, []);
 
-      //})
-    },
-    // https://en.reactjs.org/docs/faq-ajax.html
-    [] // empty deps array [] means
-       // this useEffect will run once
-       // similar to componentDidMount()
-    );
     function addItem(text){
-      console.log("addItem("+text+")")
       setInserting(true);
-      var data = {};
-      console.log(data);
-      data.description = text;
+      const data = { description: text };
       fetch(API_LIST, {
         method: 'POST',
-        // We convert the React state to JSON and send it as the POST body
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data),
-      }).then((response) => {
-        // This API doens't return a JSON document
-        console.log(response);
-        console.log();
-        console.log(response.headers.location);
-        // return response.json();
+      })
+      .then((response) => {
         if (response.ok) {
           return response;
         } else {
           throw new Error('Something went wrong ...');
         }
-      }).then(
+      })
+      .then(
         (result) => {
-          var id = result.headers.get('location');
-          var newItem = {"id": id, "description": text}
+          const id = result.headers.get('location');
+          const newItem = { id, description: text };
           setItems([newItem, ...items]);
           setInserting(false);
         },
@@ -183,71 +153,96 @@ function App() {
         }
       );
     }
+
     return (
       <div className="App">
         <h1>MY TODO LIST</h1>
-        <NewItem addItem={addItem} isInserting={isInserting}/>
-        { error &&
-          <p>Error: {error.message}</p>
-        }
-        { isLoading &&
+        <NewItem addItem={addItem} isInserting={isInserting} />
+        { error && (
+          <p className="errorMessage">Error: {error.message}</p>
+        )}
+        { isLoading && (
           <CircularProgress />
-        }
-        { !isLoading &&
+        )}
+        { !isLoading && (
         <div id="maincontent">
-        <table id="itemlistNotDone" className="itemlist">
-          <TableBody>
-          {items.map(item => (
-            !item.done && (
-            <tr key={item.id}>
-              <td className="description">{item.description}</td>
-              { /*<td>{JSON.stringify(item, null, 2) }</td>*/ }
-              <td className="deadline">
-                {item.deadline ? <Moment format="MMM Do YYYY, hh:mm A">{item.deadline}</Moment> : "No Deadline"}
-              </td>
-              <td className="date"><Moment format="MMM Do hh:mm:ss">{item.createdAt}</Moment></td>
-              <td><Button variant="contained" className="DoneButton" onClick={(event) => toggleDone(event, item.id, item.description, !item.done)} size="small">
-                    Done
-                  </Button></td>
-            </tr>
-          )))}
-          </TableBody>
-        </table>
-        <h2 id="donelist">
-          Done items
-        </h2>
-        <table id="itemlistDone" className="itemlist">
-  <TableBody>
-    {items.map(item => (
-      item.done && (
-      <tr key={item.id}>
-        <td className="description">{item.description}</td>
-        <td className="date"><Moment format="MMM Do hh:mm:ss">{item.createdAt}</Moment></td>
-        <td className="deadline">
-          {item.deadline ? <Moment format="MMM Do YYYY, hh:mm A">{item.deadline}</Moment> : "No Deadline"}
-        </td>
-        <td>
-          <Button variant="contained" className="DoneButton" 
-            onClick={(event) => toggleDone(event, item.id, item.description, !item.done)} 
-            size="small">
-            Undo
-          </Button>
-        </td>
-        <td>
-          <Button startIcon={<DeleteIcon />} variant="contained" className="DeleteButton" 
-            onClick={() => deleteItem(item.id)} size="small">
-            Delete
-          </Button>
-        </td>
-      </tr>
-    )))}
-  </TableBody>
-</table>
+          <table id="itemlistNotDone" className="itemlist">
+            <TableBody>
+              {items.map(item => (
+                !item.done && (
+                <tr key={item.id}>
+                  <td className="description">{item.description}</td>
+                  <td className="deadline">
+                    {item.deadline ? (
+                      <Moment format="MMM Do YYYY, hh:mm A">{item.deadline}</Moment>
+                    ) : (
+                      "No Deadline"
+                    )}
+                  </td>
+                  <td className="date">
+                    <Moment format="MMM Do hh:mm:ss">{item.createdAt}</Moment>
+                  </td>
+                  <td>
+                    <Button
+                      variant="contained"
+                      className="DoneButton"
+                      onClick={(event) => toggleDone(event, item.id, item.description, !item.done)}
+                      size="small"
+                    >
+                      Done
+                    </Button>
+                  </td>
+                </tr>
+              )))}
+            </TableBody>
+          </table>
 
+          <h2 id="donelist">Done items</h2>
+          <table id="itemlistDone" className="itemlist">
+            <TableBody>
+              {items.map(item => (
+                item.done && (
+                <tr key={item.id}>
+                  <td className="description">{item.description}</td>
+                  <td className="date">
+                    <Moment format="MMM Do hh:mm:ss">{item.createdAt}</Moment>
+                  </td>
+                  <td className="deadline">
+                    {item.deadline ? (
+                      <Moment format="MMM Do YYYY, hh:mm A">{item.deadline}</Moment>
+                    ) : (
+                      "No Deadline"
+                    )}
+                  </td>
+                  <td>
+                    <Button
+                      variant="contained"
+                      className="DoneButton"
+                      onClick={(event) => toggleDone(event, item.id, item.description, !item.done)}
+                      size="small"
+                    >
+                      Undo
+                    </Button>
+                  </td>
+                  <td>
+                    <Button
+                      startIcon={<DeleteIcon />}
+                      variant="contained"
+                      className="DeleteButton"
+                      onClick={() => deleteItem(item.id)}
+                      size="small"
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              )))}
+            </TableBody>
+          </table>
         </div>
-        }
-
+        )}
       </div>
     );
 }
+
 export default App;

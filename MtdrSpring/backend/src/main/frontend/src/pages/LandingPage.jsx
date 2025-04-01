7eba8a61b42386b7a0be82c1dabf73809c48ba76
Gorchon <a01254831@tec.mx>
2025-04-01 12:27:24 -0600

    Dev: Improved FE structure

---
+++ MtdrSpring/backend/src/main/frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './LandingPage.css';
+
+function LandingPage() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/home');
+  };
+
+  return (
+    <div className="landing-page">
+      <h1 className="landing-title">Welcome</h1>
+      <button className="landing-button" onClick={handleClick}>I’m a developer</button>
+      <button className="landing-button" onClick={handleClick}>I’m a manager</button>
+    </div>
+  );
+}
+
+export default LandingPage;
--- MtdrSpring/backend/src/main/frontend/src/router/AppRouter.jsx
+++ MtdrSpring/backend/src/main/frontend/src/router/AppRouter.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+import Home from '../pages/Home'; // your existing todo page
 
 function AppRouter() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        {/* Landing/welcome page is now our default path */}
+        <Route exact path="/" component={LandingPage} />
+
+        {/* Existing home (todo) page at /home */}
+        <Route path="/home" component={Home} />
+
+        {/* Add more routes as needed */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default AppRouter;

f899817af8161f7f0766eda89ac1d6313fcb507a
miguelangel <mikebarrientosb@gmail.com>
2025-04-10 07:46:22 -0600

    Cleaned all comments and unused code

--- MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/Controller/SubTareaControllerTest.java
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/Controller/SubTareaControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,15 +26,6 @@ import com.springboot.MyTodoList.service.UsuarioService;
 import com.springboot.MyTodoList.repository.*;;
 
 
-
-/*
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
- */
-
 //@SpringBootTest
 @AutoConfigureMockMvc
 @WebMvcTest(SubTareaController.class)
@@ -50,7 +41,7 @@ public class SubTareaControllerTest{
     @MockBean
     private SprintService sprintService;
     @MockBean
-    private UsuarioService usuarioService;
+    private UsuarioService usuarioService; 
 
     @MockBean
     private TareaRepository tareaRepository;  // new
@@ -64,21 +55,7 @@ public class SubTareaControllerTest{
     @Test
     public void testTSubTareas() throws Exception {
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/subtareas").accept(MediaType.APPLICATION_JSON)).andReturn();
-/*         mockMvc.perform(get("/subtareas")
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$").isArray())
-            .andExpect(jsonPath("$[0].sub_tarea_id").exists())
-            .andExpect(jsonPath("$[0].tarea_id").exists())
-            .andExpect(jsonPath("$[0].titulo").isString())
-            .andExpect(jsonPath("$[0].descripcion").isString())
-            .andExpect(jsonPath("$[0].estado").isString())
-            .andExpect(jsonPath("$[0].horas_estimadas").isNumber())
-            .andExpect(jsonPath("$[0].horas_reales").isNumber())
-            .andExpect(jsonPath("$[0].fecha_creacion").isString())
-            .andExpect(jsonPath("$[0].deadline").isString()); */
- 
+
         assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus(), "Validate endpoint return 200 OK status");
         
     }

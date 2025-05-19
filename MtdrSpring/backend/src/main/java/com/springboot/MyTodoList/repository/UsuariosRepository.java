00ef829c3c9100ba140e3822adea95a6b989ca02
Gorchon <a01254831@tec.mx>
2025-05-19 09:44:30 -0600

    Dev: Adding clean code practices

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/UsuariosRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/UsuariosRepository.java
@@ -4,8 +4,11 @@ import com.springboot.MyTodoList.model.Usuarios;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
+
   Optional<Usuarios> findByEmail(String email);
 
   Optional<Usuarios> findByTelegramChatId(Long telegramChatId);

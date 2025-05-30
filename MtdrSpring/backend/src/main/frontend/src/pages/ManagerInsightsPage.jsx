32f9f89a0ca7a64ef770d6b3b6e6e52a29faa2bd
César Silva <casrr@proton.me>
2025-05-29 19:48:54 -0600

    ff

--- MtdrSpring/backend/src/main/frontend/src/pages/ManagerInsightsPage.jsx
+++ MtdrSpring/backend/src/main/frontend/src/pages/ManagerInsightsPage.jsx
@@ -9,7 +9,7 @@ function ManagerInsightsPage() {
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" sx={{ mb: 4, fontWeight: 'bold' }}>
-        Insights del Equipo
+        Insights del Equipo This is an Error Tag
       </Typography>
       
       <Grid container spacing={4}>

8de309d5f73c5236f481f0deaaff6769c2e1bf40
César Silva <casrr@proton.me>
2025-06-09 13:39:58 -0600

    moved test

--- MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/InitializedDatabaseTest.java
+++ MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/InitializedDatabaseTest.java
@@ -52,4 +52,16 @@ public class InitializedDatabaseTest {
       assertThat(rs.getString("NOMBRE")).isEqualTo("Cesar Alan Silva Ramos");
     }
   }
+
+  /** Verifies a TAREA can be retrieved by ID and has the correct title */
+  @Test
+  void getTareaById() throws SQLException {
+    try (Connection conn = ds.getConnection();
+         Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM TAREAS WHERE TAREA_ID = 268")) {
+
+      Assertions.assertTrue(rs.next(), "No se encontró la tarea con ID = 268");
+      Assertions.assertEquals("Data Model Validation", rs.getString("TITULO"));
+    }
+  }
 }
